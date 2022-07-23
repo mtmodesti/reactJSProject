3e6a0210e9fe8e6c2bb04a1fa60a028a93de5525
mtmodesti <marcelomodesti1988@gmail.com>
2022-07-23 19:20:07 -0300

    feat: dashboard user page layout

--- src/pages/dashboardUser/index.js
+++ src/pages/dashboardUser/index.js
@@ -6,6 +6,8 @@ import ProductionQuantityLimitsIcon from "@mui/icons-material/ProductionQuantity
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import LanguageIcon from "@mui/icons-material/Language";
+import trabalheConosco from "../../assets/images/instrucoes.png";
+import "./styles.css";
 
 export const DashboardUser = () => {
   const navigate = useNavigate();
@@ -21,20 +23,23 @@ export const DashboardUser = () => {
           <img src={logo}></img>
         </LogoImg>
         <DivIcons>
-          <a href="https://panzers.com.br/produtos/" target={'_blank'}>
+          <a href="https://panzers.com.br/produtos/" target={"_blank"}>
             <ProductionQuantityLimitsIcon
               style={{ color: "white", cursor: "pointer" }}
             />
           </a>
-          <a href="https://panzers.com.br/start/" target={'_blank'}>
+          <a href="https://panzers.com.br/start/" target={"_blank"}>
             <KeyboardArrowRightIcon
               style={{ color: "white", cursor: "pointer" }}
             />
           </a>
-          <a href="https://api.whatsapp.com/send?phone=5531997616207&text=Ol%C3%A1!%20Sou%20cliente%20Panzer%20e%20preciso%20de%20ajuda" target={'_blank'}>
+          <a
+            href="https://api.whatsapp.com/send?phone=5531997616207&text=Ol%C3%A1!%20Sou%20cliente%20Panzer%20e%20preciso%20de%20ajuda"
+            target={"_blank"}
+          >
             <WhatsAppIcon style={{ color: "white", cursor: "pointer" }} />
           </a>
-          <a href="https://www.bet365.com/#/HO/" target={'_blank'}>
+          <a href="https://www.bet365.com/#/HO/" target={"_blank"}>
             <LanguageIcon style={{ color: "white", cursor: "pointer" }} />
           </a>
         </DivIcons>
@@ -42,15 +47,23 @@ export const DashboardUser = () => {
           <PowerSettingsNewIcon style={{ cursor: "pointer" }} />
         </Div>
       </AsideMenu>
-      <Main />
+      <Main>
+        <header>Bem vindo User</header>
+        <div>
+          <div>
+            <span class="trabalheConosco" src={trabalheConosco}>
+              trabalhe conosco
+            </span>
+          </div>
+          <div>
+            <div class="noticiasDireitaDiv">
+              <span class="n1"></span>
+              <span class="n2"></span>
+              <span class="n3"></span>
+            </div>
+          </div>
+        </div>
+      </Main>
     </Container>
   );
 };
-
-/* 
-https://panzers.com.br/produtos/
-https://panzers.com.br/start/
-https://api.whatsapp.com/send?phone=5531997616207&text=Ol%C3%A1!%20Sou%20cliente%20Panzer%20e%20preciso%20de%20ajuda.
-https://www.bet365.com/#/HO/
-
-*/
--- src/pages/dashboardUser/styles.js
+++ src/pages/dashboardUser/styles.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
-import LogoPic from '../../assets/images/favicon.jpg'
-
+import LogoPic from "../../assets/images/favicon.jpg";
 
 export const Container = styled.div`
   width: 100vw;
@@ -23,30 +22,80 @@ export const AsideMenu = styled.aside`
 export const Div = styled.div`
   display: flex;
   flex-direction: column;
-
-  
 `;
 export const DivIcons = styled.div`
   display: flex;
   flex-direction: column;
-height: 50%;
-justify-content: space-evenly;
-
-`
+  height: 50%;
+  justify-content: space-evenly;
+`;
 
 export const LogoImg = styled.div`
-background-image: url(${(props) => (props.src ? LogoPic : null)});
+  background-image: url(${(props) => (props.src ? LogoPic : null)});
 
-img {
+  img {
     width: 65%;
-}
-`
+  }
+`;
 
 export const Main = styled.main`
-background-color: #fff;
-height: 90%;
-width: 86%;
-border-radius: 20px;
-margin-right: 1rem;
+  background-color: #ececec;
+  height: 90%;
+  width: 86%;
+  border-radius: 20px;
+  margin-right: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
 
-`
+  header {
+    height: 10%;
+    width: 100%;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 0.2em;
+  }
+  div {
+    height: 88%;
+    width: 95%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    @media screen and (max-width: 768px) {
+      flex-direction: column;
+    }
+/* DIV ESQUERDA OU SUPERIOR */
+    div {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+    
+      @media screen and (max-width: 768px) {
+        width: 80%;
+      }
+    }
+/* DIV DIREITA OU INFERIOR */
+
+    div + div {
+
+      span {
+        height: 30%;
+        width: 100%;
+        background-color: blue;
+        margin: 10px;
+      }
+      @media screen and (max-width: 768px) {
+        width: 80%;
+      }
+
+    }
+  }
+`;
+
+
